c0ea61678e72f62f0ef405007c5f6b292031cf53
Gene <gene.levitzky@gmail.com>
2013-08-17 13:45:33 -0400

    Refactored routes, socket handling, and helper functions out of app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 /**
- *  Module dependencies. Java "import" equivalent.
+ *  Module dependencies.
  */
-var express = require('express')
-    , Character = require('./models/character').Character
-    , game = require('./modules/Game')
-    , http = require('http')
-    , io = require('socket.io')
-    , path = require('path')
-    , sha1 = require('SHA1').getSha1()
-    , User = require('./models/user').User;
+var express         = require('express')
+    , game          = require('./modules/Game')
+    , http          = require('http')
+    , H__           = require('./modules/HelperFunctions')
+    , io            = require('socket.io')
+    , routes        = require('./modules/routes')
+    , socketHandler = require('./modules/socketHandler');
 
 /**
  *  Configuration and Middleware.
@@ -40,289 +39,27 @@ app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
-// Data about the application
-var metadata = {
-    name: 'The Acts of Man'
-}
-
-
 var server = http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
 
 
-   /*************
-    *  Routes.
-    *************/
- 
-/** 
- * If the server receives a request for "/",
- * Send them index.ejs.
+/*
+ * Route Handling
  */
-app.get('/', function(req, res) {
-    indexView(req, res);
-});
+app.get('/', routes.indexView);
+app.get('/about', function(req, res) {});
+app.get('/cicero', routes.ciceroView);
+app.get('/home', routes.homeView);
+app.post('/home', routes.homeView);
+app.get('/index', routes.indexView);
+app.get('/login', routes.loginView);
+app.get('/new-character', function (req, res) {});
 
 
-/** Ditto for "/index" */
-app.get('/index', function(req, res) {
-    indexView(req, res);
-});
 
-
-app.get('/cicero', function(req, res) {
-    ciceroView(req, res);
-});
-
-
-app.get('/home', function(req, res) {
-    homeView(req, res);
-});
-
-
-app.post('/home', function(req, res) {
-    homeView(req, res);
-});
-
-
-app.get('/new-character', function (req, res) {
-
-    var userId = req.cookies.userId;
-    
-    if (!isUndefined(userId)) {
-        var user = User.findById(userId);
-        if (!isUndefined(user)) {
-            res.render('new-character', {
-                'user': user, 
-                'name': metadata.name,
-            });
-            return;
-        }
-    }
-    
-    loginView(req, res);
-});
-
-
-app.get('/login', function(req, res) {
-    loginView(req, res);
-});
-
-
-app.get('/about', function(req, res) {
-    res.render('about');
-});
-
-
-function ciceroView(req, res) {
-    res.render('cicero');
-}
-
-
-function homeView(req, res) {
-
-    if (req.param('signup')) {
-    
-        var name = req.param('name');
-        var pass = req.param('pass');
-        var passConfirm = req.param('pass-confirm');
-        
-        // Password too short
-        if (pass.length < 6) {
-            res.render('login', {   
-                'name'                : metadata.name, 
-                'loginUsernameError'  : undefined, 
-                'loginPasswordError'  : undefined,
-                'signupUsernameError' : undefined, 
-                'signupPasswordError' : 'Password must be at least 6 characters long.',
-            });
-            return;
-        }
-        
-        // Confirmation password doesn't match
-        if (pass != passConfirm) {
-            res.render('login', {   
-                'name'                : metadata.name, 
-                'loginUsernameError'  : undefined, 
-                'loginPasswordError'  : undefined,
-                'signupUsernameError' : undefined, 
-                'signupPasswordError' : 'The passwords do not match.',
-            });
-            return;
-        }
-        
-        // Username too short
-        if (name.length < 3) {
-            res.render('login', {   
-                'name'                : metadata.name, 
-                'name'                : metadata.name, 
-                'loginUsernameError'  : undefined, 
-                'loginPasswordError'  : undefined,
-                'signupUsernameError' : 'The username must be at least 3 characters.', 
-                'signupPasswordError' : undefined,
-            });
-            return;
-        }
-
-        // Username already taken
-        if (typeof User.findByName(name) !== "undefined") {
-            res.render('login', {   
-                'name'                : metadata.name, 
-                'name'                : metadata.name, 
-                'loginUsernameError'  : undefined, 
-                'loginPasswordError'  : undefined,
-                'signupUsernameError' : 'That username is already taken.', 
-                'signupPasswordError' : undefined,
-            });
-            return;
-        }
-        
-        var user = new User.construct({
-            "username" : name, 
-            "password" : sha1.hash(pass),
-        });
-        var id = user.save();
-        
-        res.cookie('userId', id, { maxAge: 900000, httpOnly: false });
-        res.render('home', {
-            'user'       : user, 
-            'characters' : user.getCharacters(), 
-            'name'       : metadata.name, 
-            'newUser'    : true,
-        });
-        
-        return;
-    }
-    else if (req.param('login')) {
-    
-        var name = req.param('name');
-        var pass = req.param('pass');
-    
-        user = User.findByName(name);
-
-        // User does not exist
-        if (isUndefined(user)) {
-            res.render('login', {   
-                'name'                : metadata.name, 
-                'loginUsernameError'  : 'That username does not exist.', 
-                'loginPasswordError'  : undefined, 
-                'signupUsernameError' : undefined, 
-                'signupPasswordError' : undefined,    
-            });
-            return;
-        }
-        
-        // Incorrect password
-        if (pass.length < 6 || sha1.hash(pass) != user.password) {
-            res.render('login', {
-                'name'                : metadata.name, 
-                'loginUsernameError'  : undefined, 
-                'loginPasswordError'  : 'Incorrect password.',
-                'signupUsernameError' : undefined, 'signupPasswordError': undefined,
-            });
-            return;
-        }
-        
-        res.cookie('userId', user.id, { maxAge: 900000, httpOnly: false });
-        res.render('home', {
-            'id'         : user.id,
-            'user'       : user, 
-            'characters' : user.getCharacters(), 
-            'name'       : metadata.name, 
-            'newUser'    : false,
-        });
-        
-        return;
-    }
-    
-    indexView(req, res);
-}
-
-
-function indexView(req, res) {
-
-    var userId = req.cookies.userId;
-    
-    if (!isUndefined(userId)) {
-        user = User.findById(userId);
-        if (!isUndefined(user)) {
-          res.render('home', {
-              'user'       : user, 
-              'characters' : user.getCharacters(), 
-              'name'       : metadata.name, 
-              'newUser'    : false,
-          });
-          return;
-        }
-    }
-    
-    loginView(req, res);
-}
-
-
-function loginView(req, res) {
-    res.render('Login', {
-        'name'                : metadata.name, 
-        'loginUsernameError'  : undefined, 
-        'loginPasswordError'  : undefined,
-        'signupUsernameError' : undefined, 
-        'signupPasswordError' : undefined,
-    });
-}
-
-      /************
-       *  SOCKETS
-       ************/
-
+/*
+ * Socket Handling
+ */
 var sio = io.listen(server);
-       
-sio.on('connection', function (socket) {
-
-    socket.emit('who-are-you?');
-    
-    
-    socket.on('i-am', function (data) {
-        
-        if (isUndefined(data)) {
-            // TODO
-            socket.emit('not-logged-in');
-            console.log("USER NOT LOGGED IN");
-            return;
-        }
-        else {
-        
-            var user = User.findById(data.userId);
-            
-            if (isUndefined(user)) {
-                console.log("INVALID USER");
-                return;
-            }
-            else {   
-            
-                if (isUndefined(data.service)) {
-                    socket.emit('no-service-requested');
-                }
-                else {
-                
-                    if ('login' == data.service) {
-                        var character = Character.findById(0);
-                        game.createGameSession(character, socket);
-                    }
-                    else {
-                        // TODO
-                        console.log("ILLEGAL SERVICE REQUESTED");
-                        return;
-                    }
-                }
-            }
-        }
-    });
-});
-
-
-
-/** HELPER FUNCTIONS **/
-
-function isUndefined (obj) {
-    return typeof obj === "undefined";
-}
+socketHandler.init(sio);
---
+++ modules/socketHandler/index.js
@@ -0,0 +1,52 @@
+
+var Character = require('../../models/character').Character
+    , game    = require('../Game')
+    , H__     = require('../HelperFunctions')
+    , User    = require('../../models/user').User;
+
+    
+exports.init = function(sio) {
+
+    sio.on('connection', function (socket) {
+
+        // Request authentication
+        socket.emit('who-are-you?');
+        
+        // Respond to authentication
+        socket.on('i-am', function (data) {
+            // No authentication data sent to server
+            if (H__.isUndefined(data)) {
+                socket.emit('not-logged-in');
+                console.log("USER NOT LOGGED IN");
+                return;
+            }
+            else {
+            
+                var user = User.findById(data.userId);
+                // Invalid User ID
+                if (H__.isUndefined(user)) {
+                    console.log("INVALID USER");
+                    return;
+                }
+                else {   
+                    // No service requested
+                    if (H__.isUndefined(data.service)) {
+                        socket.emit('no-service-requested');
+                    }
+                    else {
+                        // Login requested
+                        if ('login' == data.service) {
+                            var character = Character.findById(0);
+                            game.createGameSession(character, socket);
+                        }
+                        else {
+                            // Non-existent service requested
+                            console.log("ILLEGAL SERVICE REQUESTED");
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+    });
+}
